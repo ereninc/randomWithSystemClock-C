35337521331b3f4ad710e88a68c0c6aa4cd82e82
Eren <erencan_inanci@hotmail.com>
2021-02-09 18:24:33 +0300

    Update Random.c

--- src/Random.c
+++ src/Random.c
@@ -7,7 +7,7 @@
 
 /**
 *
-* @author Erencan İNANCI - erencan_inanci@hotmail.com / b171210308@sakarya.edu.tr
+* @author Eren
 * @since 12.04.2019 / 13:54:34
 * <p>
 * Anlık milisaniyeyi alan fonksiyonumuzun olduğu dosyamız.
